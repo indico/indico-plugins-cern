c9e18858a5cb35490b5f4f6b6934b55c5839997a
Adrian Moennich <adrian.moennich@cern.ch>
2014-11-17 16:41:01 +0100

    Add agent config form

--- livesync_cern/indico_livesync_cern/agent.py
+++ livesync_cern/indico_livesync_cern/agent.py
@@ -1,6 +1,22 @@
 from __future__ import unicode_literals
 
+from wtforms.fields.core import StringField
+from wtforms.fields.html5 import URLField
+from wtforms.validators import DataRequired, URL
+
+from indico.util.i18n import _
+from indico.util.string import strip_whitespace
+from indico.web.forms.fields import UnsafePasswordField
+
 from indico_livesync import LiveSyncAgentBase, MARCXMLUploader
+from indico_livesync import AgentForm
+
+
+class CERNAgentForm(AgentForm):
+    server_url = URLField(_('URL'), [DataRequired(), URL(require_tld=False)], filters=[strip_whitespace],
+                          description=_("The URL of CERNsearch's import endpoint"))
+    username = StringField(_('Username'), [DataRequired()], filters=[strip_whitespace])
+    password = UnsafePasswordField(_('Password'), [DataRequired()], filters=[strip_whitespace])
 
 
 class CERNUploader(MARCXMLUploader):
@@ -15,3 +31,4 @@ class CERNLiveSyncAgent(LiveSyncAgentBase):
     """
 
     uploader = CERNUploader
+    form = CERNAgentForm
