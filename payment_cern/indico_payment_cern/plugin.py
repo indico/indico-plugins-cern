c77fb2588a2537df8c7f89704bb183f0ff834033
Adrian Moennich <adrian.moennich@cern.ch>
2015-01-05 15:23:38 +0100

    Payment/CERN: Restrict plugin access
    - Only Indico admins and authorized users can access it.
    - But those people can do so even if they don't have access to manage
      the event itself.

--- payment_cern/indico_payment_cern/plugin.py
+++ payment_cern/indico_payment_cern/plugin.py
@@ -7,9 +7,14 @@ from wtforms.validators import DataRequired
 from indico.core.plugins import IndicoPlugin
 from indico.modules.payment import PaymentPluginMixin, PaymentPluginSettingsFormBase, PaymentEventSettingsFormBase
 from indico.util.i18n import _
+from indico.util.user import retrieve_principals
+from indico.web.forms.fields import PrincipalField
 
 
 class PluginSettingsForm(PaymentPluginSettingsFormBase):
+    authorized_users = PrincipalField(_('Authorized users'), groups=True,
+                                      description=_('List of users/groups who are authorized to configure the CERN '
+                                                    'Payment module for any event.'))
     fp_email_address = EmailField(_('FP email adress'), [DataRequired()], description=_('Email address to contact FP.'))
     fp_department_name = StringField(_('FP department name'), [DataRequired()])
     payment_url = URLField(_('Payment URL'), [DataRequired()], description=_('URL used for the epayment'))
@@ -17,9 +22,8 @@ class PluginSettingsForm(PaymentPluginSettingsFormBase):
     master_shop_id = StringField(_('Master shop ID'), [DataRequired()])
     hash_seed = StringField(_('Hash seed'), [DataRequired()])
     hash_seed_out = StringField(_('Hash seed out'), [DataRequired()])
-    server_url_suffix = StringField(_('Server URL Suffix'), [DataRequired()],
-                                    description='Server URL Suffix (indico[suffix].cern.ch)')
-    order_id_prefix = StringField(_('Order ID Prefix'), [DataRequired()])
+    server_url_suffix = StringField(_('Server URL Suffix'), description='Server URL Suffix (indico[suffix].cern.ch)')
+    order_id_prefix = StringField(_('Order ID Prefix'))
     # TODO: Add payment methods
 
 
@@ -33,3 +37,9 @@ class CERNPaymentPlugin(PaymentPluginMixin, IndicoPlugin):
     settings_form = PluginSettingsForm
     event_settings_form = EventSettingsForm
     default_settings = {'method_name': 'CERN YellowPay'}
+
+    def can_be_modified(self, user, event):
+        if user.isAdmin():
+            return True
+        authorized_users = retrieve_principals(self.settings.get('authorized_users'))
+        return any(principal.containsUser(user) for principal in authorized_users)
