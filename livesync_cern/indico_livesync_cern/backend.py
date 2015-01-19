2f3b856988fee0c6dde047d0f01fdc1b66774104
Alejandro Avilés <aavilesd@cern.ch>
2015-01-19 17:11:52 +0100

    Livesync/CERN: Adapt to APIs

--- livesync_cern/indico_livesync_cern/backend.py
+++ livesync_cern/indico_livesync_cern/backend.py
@@ -25,21 +25,21 @@ class CERNUploaderError(Exception):
 
 
 class CERNUploader(MARCXMLUploader):
-    def __init__(self, backend):
-        super(CERNUploader, self).__init__(backend)
+    def __init__(self, *args, **kwargs):
+        super(CERNUploader, self).__init__(*args, **kwargs)
         self.url = self.backend.agent.settings.get('server_url')
         self.username = self.backend.agent.settings.get('username')
         self.password = self.backend.agent.settings.get('password')
 
     def upload_xml(self, xml):
         response = requests.post(self.url, auth=(self.username, self.password), data={'xml': xml})
-        result_text = self._get_result_text(response.text)
+        result_text = self._get_result_text(response.content)
 
         if response.status_code != 200 or result_text != 'true':
             raise CERNUploaderError('{} - {}'.format(response.status_code, result_text))
 
     def _get_result_text(self, result):
-        return etree.tostring(etree.fromstring(result.read()), method="text")
+        return etree.tostring(etree.fromstring(result), method="text")
 
 
 class CERNLiveSyncBackend(LiveSyncBackendBase):
