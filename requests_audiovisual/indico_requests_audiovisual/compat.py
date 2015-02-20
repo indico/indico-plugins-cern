059809e047a7b9495a1b96ce1ed572b510c0aae2
Adrian Moennich <adrian.moennich@cern.ch>
2015-02-20 10:24:50 +0100

    Requests/Audiovisual: Redirect from old agreement URL

--- requests_audiovisual/indico_requests_audiovisual/compat.py
+++ requests_audiovisual/indico_requests_audiovisual/compat.py
@@ -7,6 +7,8 @@ from indico.core.plugins import IndicoPluginBlueprint
 from indico.modules.events.agreements.models.agreements import Agreement
 from indico.web.flask.util import url_for
 
+from indico_requests_audiovisual.definition import SpeakerReleaseAgreement
+
 
 compat_blueprint = IndicoPluginBlueprint('compat_requests_audiovisual', 'indico_requests_audiovisual')
 
@@ -18,3 +20,9 @@ def redirect_old_agreement_url(confId):
     if agreement is None:
         raise NotFound
     return redirect(url_for('agreements.agreement_form', confId=confId, id=agreement.id, uuid=uuid))
+
+
+@compat_blueprint.route('/export/eAgreements/<event_id>.<ext>')
+def redirect_old_eagreement_api(event_id, ext):
+    path = 'agreements/{}/{}.{}'.format(SpeakerReleaseAgreement.name, event_id, ext)
+    return redirect(url_for('api.httpapi', prefix='export', path=path, **request.args.to_dict()))
