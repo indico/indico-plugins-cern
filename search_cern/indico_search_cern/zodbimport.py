7abdcda1cdbbe028178c3adfdb47d7f1ec65be36
Jacques Dafflon <jacques.dafflon@cern.ch>
2015-03-02 16:15:24 +0100

    Search/CERN: update zodbimport to use option_value

--- search_cern/indico_search_cern/zodbimport.py
+++ search_cern/indico_search_cern/zodbimport.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 from indico.core.db import db
 from indico.util.console import cformat
-from indico_zodbimport import Importer, convert_to_unicode
+from indico_zodbimport import Importer, option_value
 
 from indico_search_cern.plugin import CERNSearchPlugin
 
@@ -17,5 +17,5 @@ class CERNSearchImporter(Importer):
         print cformat('%{white!}migrating settings')
         CERNSearchPlugin.settings.delete_all()
         opts = self.zodb_root['plugins']['search']._PluginType__plugins['cern_search']._PluginBase__options
-        CERNSearchPlugin.settings.set('search_url', convert_to_unicode(opts['serverUrl']._PluginOption__value).strip())
+        CERNSearchPlugin.settings.set('search_url', option_value(opts['serverUrl']).strip())
         db.session.commit()
