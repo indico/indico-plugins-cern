5f449b345be226e437b04405bf6a5f2bf5e56ff8
Adrian Moennich <adrian.moennich@cern.ch>
2017-03-24 13:22:02 +0100

    CustomThemes: Initial import (CERN/LCAgenda)

---
+++ custom_themes/indico_custom_themes/plugins.py
@@ -0,0 +1,40 @@
+from __future__ import unicode_literals
+
+import os
+
+from flask.helpers import get_root_path
+
+from indico.core import signals
+from indico.core.plugins import IndicoPlugin, IndicoPluginBlueprint
+
+
+class ThemesPluginBase(IndicoPlugin):
+    THEME_YAML = None
+
+    def init(self):
+        super(ThemesPluginBase, self).init()
+        self.connect(signals.plugin.get_event_themes_files, self._get_themes_yaml)
+
+    def get_blueprints(self):
+        return IndicoPluginBlueprint(self.name, __name__)
+
+    def _get_themes_yaml(self, sender, **kwargs):
+        return os.path.join(get_root_path('indico_custom_themes'), self.THEME_YAML)
+
+
+class LCAgendaThemesPlugin(ThemesPluginBase):
+    """LCAgenda Themes
+
+    Provides event themes for LCAgenda.
+    """
+
+    THEME_YAML = 'themes-lcagenda.yaml'
+
+
+class CERNThemesPlugin(ThemesPluginBase):
+    """CERN Themes
+
+    Provides event themes for CERN.
+    """
+
+    THEME_YAML = 'themes-cern.yaml'
---
+++ custom_themes/setup.py
@@ -0,0 +1,19 @@
+from __future__ import unicode_literals
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='indico_custom_themes',
+    version='1.0.0',
+    url='https://gitlab.cern.ch/indico/indico-plugins-cern',
+    author='Indico Team',
+    author_email='indico-team@cern.ch',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=['indico>=1.9.10.dev0'],
+    entry_points={'indico.plugins': {'themes_cern = indico_custom_themes.plugins:CERNThemesPlugin',
+                                     'themes_lcagenda = indico_custom_themes.plugins:LCAgendaThemesPlugin'}}
+)
