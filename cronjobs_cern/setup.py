b3bee83d349d6672669a3eb520c64ee8ca5e722a
Pedro Ferreira <pedro.ferreira@cern.ch>
2017-04-10 20:08:41 +0200

    Add MANIFEST.in
    Also require final 1.9.10

--- cronjobs_cern/setup.py
+++ cronjobs_cern/setup.py
@@ -14,7 +14,7 @@ setup(
     include_package_data=True,
     platforms='any',
     install_requires=[
-        'indico>=1.9.10.dev3'
+        'indico>=1.9.10'
     ],
     entry_points={'indico.plugins': {'cronjobs_cern = indico_cronjobs_cern.plugin:CERNCronjobsPlugin'}}
 )
