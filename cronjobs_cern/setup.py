7eec5d75411260f1a8d606cefe4113223ace7ef2
Ilias Trichopoulos <ilias.trichopoulos@cern.ch>
2017-04-11 15:05:45 +0200

    CERN/Cronjobs: Bump version

--- cronjobs_cern/setup.py
+++ cronjobs_cern/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='indico_cronjobs_cern',
-    version='0.1.0',
+    version='0.1.1',
     url='https://gitlab.cern.ch/indico/indico-plugins-cern',
     author='Indico Team',
     author_email='indico-team@cern.ch',
