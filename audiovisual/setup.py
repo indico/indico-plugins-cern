90bbe07487d0cec48a39530057bc6adc1758fab3
Adrian Moennich <adrian.moennich@cern.ch>
2017-05-15 18:13:13 +0200

    Adapt to event locking changes

--- audiovisual/setup.py
+++ audiovisual/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='indico_audiovisual',
-    version='0.4.1',
+    version='0.4.2',
     url='https://gitlab.cern.ch/indico/indico-plugins-cern',
     author='Indico Team',
     author_email='indico-team@cern.ch',
@@ -13,7 +13,7 @@ setup(
     zip_safe=False,
     include_package_data=True,
     install_requires=[
-        'indico>=1.9.11.dev1'
+        'indico>=1.9.11.dev2'
     ],
     entry_points={
         'indico.plugins': {'audiovisual = indico_audiovisual.plugin:AVRequestsPlugin'},
--- custom_themes/setup.py
+++ custom_themes/setup.py
@@ -5,14 +5,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='indico_custom_themes',
-    version='0.1.1',
+    version='0.1.2',
     url='https://gitlab.cern.ch/indico/indico-plugins-cern',
     author='Indico Team',
     author_email='indico-team@cern.ch',
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
-    install_requires=['indico>=1.9.10'],
+    install_requires=['indico>=1.9.11.dev2'],
     entry_points={'indico.plugins': {'themes_cern = indico_custom_themes.plugins:CERNThemesPlugin',
                                      'themes_lcagenda = indico_custom_themes.plugins:LCAgendaThemesPlugin'}}
 )
