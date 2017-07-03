7b4a425108a2f64c42e433adc3246a16bb8398f7
David Andreev <david.andreev@cern.ch>
2017-07-03 12:11:10 +0200

    Payment/CERN: Fix unicode error
    remove_accents does not convert e.g. the "polish L" with the slash
    through it, so we ended up with a non-ascii bytestring

--- payment_cern/setup.py
+++ payment_cern/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='indico_payment_cern',
-    version='0.3.1',
+    version='0.3.2',
     url='https://gitlab.cern.ch/indico/indico-plugins-cern',
     author='Indico Team',
     author_email='indico-team@cern.ch',
