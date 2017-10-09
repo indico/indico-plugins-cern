ac10b1519fa5acb91142cf0435645adef3867d6b
Adrian Moennich <adrian.moennich@cern.ch>
2017-10-09 20:55:59 +0200

    Audiovisual: Adapt to new breadcrumbs

--- audiovisual/indico_audiovisual/views.py
+++ audiovisual/indico_audiovisual/views.py
@@ -2,12 +2,14 @@ from __future__ import unicode_literals
 
 from indico.core.plugins import WPJinjaMixinPlugin
 from indico.legacy.webinterface.pages.base import WPDecorated
-from indico.legacy.webinterface.wcomponents import WSimpleNavigationDrawer
+from indico.web.breadcrumbs import render_breadcrumbs
+
+from indico_audiovisual import _
 
 
 class WPAudiovisualManagers(WPJinjaMixinPlugin, WPDecorated):
-    def _getNavigationDrawer(self):
-        return WSimpleNavigationDrawer('Webcast/Recording')
+    def _get_breadcrumbs(self):
+        return render_breadcrumbs(_('Webcast/Recording'))
 
     def _getBody(self, params):
         return self._getPageContent(params)
