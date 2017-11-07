04f240155a8140509db5ca57b8fd1dbde3ee3890
Adrian Moennich <adrian.moennich@cern.ch>
2017-11-07 17:49:47 +0100

    Add MIT license headers

--- audiovisual/indico_audiovisual/plugin.py
+++ audiovisual/indico_audiovisual/plugin.py
@@ -1,3 +1,10 @@
+# This file is part of the CERN Indico plugins.
+# Copyright (C) 2014 - 2017 CERN
+#
+# The CERN Indico plugins are free software; you can redistribute
+# them and/or modify them under the terms of the MIT License; see
+# the LICENSE file for more details.
+
 from __future__ import unicode_literals
 
 from flask import g, request, session
--- ravem/indico_ravem/api.py
+++ ravem/indico_ravem/api.py
@@ -1,3 +1,10 @@
+# This file is part of the CERN Indico plugins.
+# Copyright (C) 2014 - 2017 CERN
+#
+# The CERN Indico plugins are free software; you can redistribute
+# them and/or modify them under the terms of the MIT License; see
+# the LICENSE file for more details.
+
 from indico_ravem.util import ravem_api_call
 
 
