2f437144ea8dffc143c166885d3af215894780de
Adrian Moennich <adrian.moennich@cern.ch>
2017-11-08 14:06:41 +0100

    CERNAccess: Update setup.py & add license headers

--- cern_access/setup.py
+++ cern_access/setup.py
@@ -1,3 +1,17 @@
+# This file is part of the CERN Indico plugins.
+# Copyright (C) 2014 - 2017 CERN
+#
+# The CERN Indico plugins are free software; you can redistribute
+# them and/or modify them under the terms of the MIT License; see
+# the LICENSE file for more details.
+
+# This file is part of the CERN Indico plugins.
+# Copyright (C) 2014 - 2017 CERN
+#
+# The CERN Indico plugins are free software; you can redistribute
+# them and/or modify them under the terms of the MIT License; see
+# the LICENSE file for more details.
+
 from __future__ import unicode_literals
 
 from setuptools import find_packages, setup
@@ -6,17 +20,23 @@ from setuptools import find_packages, setup
 setup(
     name='indico-plugin-cern-access',
     version='1.0.dev0',
-    url='https://gitlab.cern.ch/indico/indico-plugins-cern',
+    url='https://github.com/indico/indico-plugins-cern',
+    license='MIT',
     author='Indico Team',
     author_email='indico-team@cern.ch',
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
-    platforms='any',
     install_requires=[
         'indico>=2.0a2'
     ],
     entry_points={
         'indico.plugins': {'cern_access = indico_cern_access.plugin:CERNAccessPlugin'}
-    }
+    },
+    classifiers=[
+        'Environment :: Plugins',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
 )
