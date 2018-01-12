265a65df2c6bbbd8d4cfa5218207293fb04fd881
Indico Team <indico-team@cern.ch>
2018-01-12 16:09:23 +0100

    Happy new year 2018 :fireworks:

--- cern_access/setup.py
+++ cern_access/setup.py
@@ -1,12 +1,11 @@
 # This file is part of the CERN Indico plugins.
-# Copyright (C) 2014 - 2017 CERN
+# Copyright (C) 2014 - 2018 CERN
 #
 # The CERN Indico plugins are free software; you can redistribute
 # them and/or modify them under the terms of the MIT License; see
 # the LICENSE file for more details.
-
 # This file is part of the CERN Indico plugins.
-# Copyright (C) 2014 - 2017 CERN
+# Copyright (C) 2014 - 2018 CERN
 #
 # The CERN Indico plugins are free software; you can redistribute
 # them and/or modify them under the terms of the MIT License; see
