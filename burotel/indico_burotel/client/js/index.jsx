2bb94c8095d7b6a89f3c9ef23268ae05d7622f12
Pedro Ferreira <pedro.ferreira@cern.ch>
2018-10-01 17:18:14 +0200

    Burotel: parametrize landing statistics and menu

--- burotel/indico_burotel/client/js/index.jsx
+++ burotel/indico_burotel/client/js/index.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 import setup from 'indico/modules/rb_new/setup';
 import DefaultApp from 'indico/modules/rb_new/containers/App';
 import DefaultBookingBootstrapForm from 'indico/modules/rb_new/components/BookingBootstrapForm';
+import DefaultMenu from 'indico/modules/rb_new/components/Menu';
 import DefaultBookingFilterBar from 'indico/modules/rb_new/modules/bookRoom/BookingFilterBar';
 import {TimeInformation as DefaultTimeInformation} from 'indico/modules/rb_new/modules/bookRoom/BookRoomModal';
 import DefaultBookRoom from 'indico/modules/rb_new/modules/bookRoom/BookRoom';
+import DefaultLandingStatistics from 'indico/modules/rb_new/modules/landing/LandingStatistics';
 import {Translate} from 'indico/react/i18n';
 import BootstrapOptions from './BootstrapOptions';
 import ExtraFilters from './ExtraFilters';
@@ -51,12 +53,27 @@ const TimeInformation = parametrize(DefaultTimeInformation, {
     timeSlot: null
 });
 
+const LandingStatistics = parametrize(DefaultLandingStatistics, {
+    labels: {
+        activeRooms: <Translate>Desks in use</Translate>
+    }
+});
+
+const Menu = parametrize(DefaultMenu, {
+    labels: {
+        bookRoom: <Translate>Book a Desk</Translate>,
+        roomList: <Translate>List of Spaces</Translate>
+    }
+})
+
 setup({
     App,
     BookingBootstrapForm,
     BookingFilterBar,
     BookRoom,
     TimeInformation,
+    LandingStatistics,
+    Menu,
     'Landing.bootstrapOptions': BootstrapOptions,
     'RoomFilterBar.extraFilters': ExtraFilters
 });
