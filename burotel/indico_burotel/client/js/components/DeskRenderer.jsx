a9bfa350ae54d8e58166754d463d852766b4d71e
Pedro Ferreira <pedro.ferreira@cern.ch>
2018-11-15 14:33:02 +0100

    Burotel: make map highlighting on hover work

--- burotel/indico_burotel/client/js/components/DeskRenderer.jsx
+++ burotel/indico_burotel/client/js/components/DeskRenderer.jsx
@@ -2,16 +2,17 @@ import _ from 'underscore';
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Icon, Card} from 'semantic-ui-react';
+import {withHoverListener} from 'indico/modules/rb_new/common/map/util';
 import {Slot} from 'indico/react/util';
 
 import './DeskRenderer.module.scss';
 
 
-function Desk({desk: {name, division}, desk, actionRenderer}) {
+function Desk({desk: {name, division}, desk, actionRenderer, ...rest}) {
     const {actions} = Slot.split(actionRenderer(desk));
 
     return (
-        <Card>
+        <Card {...rest}>
             <Card.Content>
                 <Card.Header>
                     {name}
@@ -33,11 +34,15 @@ Desk.propTypes = {
 };
 
 function RoomRenderer({id, desks, actionRenderer}) {
+    const DeskComponent = withHoverListener(({room: desk, ...rest}) => (
+        <Desk key={desk.id} desk={desk} actionRenderer={actionRenderer} {...rest} />
+    ));
+
     return (
         <div styleName="room">
             <h3>{id}</h3>
             <Card.Group styleName="room-container">
-                {desks.map(d => <Desk key={d.id} desk={d} actionRenderer={actionRenderer} />)}
+                {desks.map(d => <DeskComponent key={d.id} room={d} />)}
             </Card.Group>
         </div>
     );
