5e00c7e5e6fba7dde64f5210de91f043f93a8349
Michal Kolodziejski <michal.kolodziejski@cern.ch>
2019-02-20 11:52:05 +0100

    RoomAssistance: Add models and migration

---
+++ startup_assistance/indico_startup_assistance/migrations/20190220_1138_b160812c186b_add_startup_assistance_requests_table.py
@@ -0,0 +1,68 @@
+"""Add startup assistance requests table
+
+Revision ID: b160812c186b
+Revises:
+Create Date: 2019-02-20 11:38:08.691528
+"""
+
+from __future__ import unicode_literals
+
+import sqlalchemy as sa
+from alembic import context, op
+from sqlalchemy.sql.ddl import CreateSchema, DropSchema
+
+
+# revision identifiers, used by Alembic.
+revision = 'b160812c186b'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    if context.is_offline_mode():
+        raise Exception('This upgrade is only possible in online mode')
+
+    op.execute(CreateSchema('plugin_startup_assistance'))
+    op.create_table(
+        'startup_assistance_requests',
+        sa.Column('reservation_id', sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(['reservation_id'], ['roombooking.reservations.id']),
+        sa.PrimaryKeyConstraint('reservation_id'),
+        schema='plugin_startup_assistance'
+    )
+
+    conn = op.get_bind()
+    res = conn.execute('''
+        SELECT id FROM roombooking.reservations
+        WHERE needs_assistance
+        AND end_dt > NOW()::TIMESTAMP
+        AND state IN (1, 2)
+    ''')
+    for row in res:
+        conn.execute('''
+            INSERT INTO plugin_startup_assistance.startup_assistance_requests
+            VALUES (%s)
+        ''', (row.id,))
+
+    op.drop_column('reservations', 'needs_assistance', schema='roombooking')
+
+
+def downgrade():
+    if context.is_offline_mode():
+        raise Exception('This downgrade is only possible in online mode')
+
+    op.add_column('reservations',
+                  sa.Column('needs_assistance', sa.Boolean(), nullable=False, server_default='false'),
+                  schema='roombooking')
+
+    conn = op.get_bind()
+    res = conn.execute('SELECT reservation_id FROM plugin_startup_assistance.startup_assistance_requests')
+    for row in res:
+        resv = conn.execute('SELECT TRUE FROM roombooking.reservations WHERE id = %s', (row.reservation_id,)).scalar()
+        if not bool(resv):
+            continue
+        conn.execute('UPDATE roombooking.reservations SET needs_assistance = TRUE WHERE id = %s', (row.reservation_id,))
+
+    op.drop_table('startup_assistance_requests', schema='plugin_startup_assistance')
+    op.execute(DropSchema('plugin_startup_assistance'))
---
+++ startup_assistance/indico_startup_assistance/models/startup_assistance_requests.py
@@ -0,0 +1,37 @@
+# This file is part of the CERN Indico plugins.
+# Copyright (C) 2014 - 2018 CERN
+#
+# The CERN Indico plugins are free software; you can redistribute
+# them and/or modify them under the terms of the MIT License; see
+# the LICENSE file for more details.
+
+from __future__ import unicode_literals
+
+from indico.core.db.sqlalchemy import db
+from indico.util.string import format_repr
+
+
+class StartupAssistanceRequest(db.Model):
+    """Startup assistance requests"""
+
+    __tablename__ = 'startup_assistance_requests'
+    __table_args__ = {'schema': 'plugin_startup_assistance'}
+
+    reservation_id = db.Column(
+        db.ForeignKey('roombooking.reservations.id'),
+        primary_key=True
+    )
+
+    reservation = db.relationship(
+        'Reservation',
+        uselist=False,
+        lazy=False,
+        backref=db.backref(
+            'startup_assistance_request',
+            uselist=False,
+            lazy=True
+        )
+    )
+
+    def __repr__(self):
+        return format_repr(self, 'reservation_id')
