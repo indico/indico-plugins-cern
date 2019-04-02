3b20809786fe52f60063603ea6cca92997e8da08
Marco Vidal Garcia <marco.vidal@cern.ch>
2019-04-02 16:33:10 +0200

    Burotel: Add gutter to long term desks in timeline

---
+++ burotel/indico_burotel/client/js/components/RowLabel.jsx
@@ -0,0 +1,27 @@
+/* This file is part of the CERN Indico plugins.
+ * Copyright (C) 2014 - 2019 CERN
+ *
+ * The CERN Indico plugins are free software; you can redistribute
+ * them and/or modify them under the terms of the MIT License; see
+ * the LICENSE file for more details.
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {Translate} from 'indico/react/i18n';
+
+import {TimelineRowLabel} from 'indico/modules/rb_new/common/timeline/DailyTimelineContent';
+
+
+const RowLabel = ({room, ...props}) => (
+    <TimelineRowLabel {...props}
+                      gutter={room.isLongTerm}
+                      gutterTooltip={Translate.string('Long term')} />
+);
+
+RowLabel.propTypes = {
+    room: PropTypes.object.isRequired,
+};
+
+export default RowLabel;
--- burotel/indico_burotel/client/js/overrides.js
+++ burotel/indico_burotel/client/js/overrides.js
@@ -3,7 +3,7 @@ import BootstrapOptions from './components/BootstrapOptions';
 import ExtraFilters from './components/ExtraFilters';
 import DailyButton from './components/DailyButton';
 import BurotelLanding from './components/BurotelLanding';
-
+import RowLabel from './components/RowLabel';
 
 export default {
     'Landing': BurotelLanding,
@@ -11,4 +11,5 @@ export default {
     'Landing.bootstrapOptions': BootstrapOptions,
     'RoomFilterBar.extraFilters': ExtraFilters,
     'BookingFilterBar.recurrence': DailyButton,
+    'TimelineContent.rowLabel': RowLabel,
 };
