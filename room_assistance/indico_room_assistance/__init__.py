0daaca41b917c6d1de1b1e5ef493afaf552ea62a
Michal Kolodziejski <michal.kolodziejski@cern.ch>
2019-04-09 12:06:00 +0200

    RoomAssistance: Properly register celery tasks

--- room_assistance/indico_room_assistance/__init__.py
+++ room_assistance/indico_room_assistance/__init__.py
@@ -7,7 +7,13 @@
 
 from __future__ import unicode_literals
 
+from indico.core import signals
 from indico.util.i18n import make_bound_gettext
 
 
 _ = make_bound_gettext('room_assistance')
+
+
+@signals.import_tasks.connect
+def _import_tasks(sender, **kwargs):
+    import indico_room_assistance.tasks
