de34b5e3b2a66d2a9f43071732fe0c27e208b587
Michal Kolodziejski <michal.kolodziejski@cern.ch>
2019-06-17 08:28:39 +0200

    RoomAssistance: Modify assistance task to use new schema

--- room_assistance/indico_room_assistance/tasks.py
+++ room_assistance/indico_room_assistance/tasks.py
@@ -9,6 +9,8 @@ from __future__ import unicode_literals
 
 import dateutil.parser
 from celery.schedules import crontab
+from sqlalchemy import func
+from sqlalchemy.orm import aliased
 
 from indico.core.celery import celery
 from indico.core.config import config
@@ -24,13 +26,20 @@ from indico_room_assistance.plugin import RoomAssistancePlugin
 
 @celery.periodic_task(run_every=crontab(minute='0', hour='6'), plugin='room_assistance')
 def room_assistance_emails():
-    requests = (Request.query
-                .filter(Request.type == 'room-assistance',
-                        Request.state == RequestState.accepted,
-                        Event.own_room_id.isnot(None),
-                        db.cast(Request.data['start_dt'].astext, db.Date) == db.cast(now_utc(), db.Date))
-                .join(Event))
+    inner = (Request.query
+             .filter(Request.type == 'room-assistance',
+                     Request.state == RequestState.accepted)
+             .add_columns(func.jsonb_array_elements(Request.data['occurrences']).label('requested_at'))
+             .subquery())
 
+    aliased_event = aliased(Event, name='event')
+    requests = (db.session.query(inner, aliased_event)
+                .join(aliased_event, aliased_event.id == inner.c.event_id)
+                .filter(aliased_event.own_room_id.isnot(None))
+                .filter(db.cast(db.cast(inner.c.requested_at, db.String), db.Date) == db.cast(now_utc(), db.Date))
+                .all())
+
+    requests = [req._asdict() for req in requests]
     template = get_plugin_template_module('emails/room_assistance_emails.html', requests=requests,
                                           parse_dt=dateutil.parser.parse)
     recipients = RoomAssistancePlugin.settings.get('room_assistance_recipients')
