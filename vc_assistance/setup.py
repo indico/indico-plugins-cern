869cc8cda857a51c1111191faf569abb3a667e9b
Adrian Moennich <adrian.moennich@cern.ch>
2019-07-29 10:36:37 +0200

    Bump 2.2-dev versions to 2.2

--- vc_assistance/setup.py
+++ vc_assistance/setup.py
@@ -12,7 +12,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='indico-plugin-vc-assistance',
-    version='2.2-dev',
+    version='2.2',
     url='https://github.com/indico/indico-plugins-cern',
     license='MIT',
     author='Indico Team',
