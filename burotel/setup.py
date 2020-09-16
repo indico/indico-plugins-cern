13170689b82ae482657b98f8be5f53449f8d15fe
Adrian Moennich <adrian.moennich@cern.ch>
2020-09-16 14:54:28 +0200

    Burotel: Bump version to 2.3

--- burotel/setup.py
+++ burotel/setup.py
@@ -12,7 +12,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='indico-plugin-burotel',
-    version='2.3-dev',
+    version='2.3',
     url='https://github.com/indico/indico-plugins-cern',
     license='MIT',
     author='Indico Team',
