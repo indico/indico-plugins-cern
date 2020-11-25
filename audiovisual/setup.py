2cc52c7a0413e3e468aa751613515a7b323a6679
Adrian Moennich <adrian.moennich@cern.ch>
2020-11-25 09:30:58 +0100

    Audiovisual: Fix deleting events with active request
    When those events got deleted from a category cleanup task there is no
    request context so getting the timezone from he session fails.

--- audiovisual/setup.py
+++ audiovisual/setup.py
@@ -12,7 +12,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='indico-plugin-audiovisual',
-    version='2.3',
+    version='2.3.1',
     url='https://github.com/indico/indico-plugins-cern',
     license='MIT',
     author='Indico Team',
