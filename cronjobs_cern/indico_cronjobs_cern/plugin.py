18d422cf29ef38141a2589a73781839d9a34b82b
Björn Pedersen <bjoern.pedersen@frm2.tum.de>
2021-07-22 17:15:22 +0200

    Cronjobs/CERN: Make sender email configurable

--- cronjobs_cern/indico_cronjobs_cern/plugin.py
+++ cronjobs_cern/indico_cronjobs_cern/plugin.py
@@ -5,6 +5,9 @@
 # them and/or modify them under the terms of the MIT License; see
 # the LICENSE file for more details.
 
+from wtforms.fields import EmailField
+from wtforms.validators import DataRequired, Email
+
 from indico.core.plugins import IndicoPlugin
 from indico.util.string import natural_sort_key
 from indico.web.forms.base import IndicoForm
@@ -17,9 +20,11 @@ def _order_func(object_list):
 
 class SettingsForm(IndicoForm):
     _fieldsets = [
+        ('General settings', ['sender_email']),
         ('Seminar emails', ['seminar_categories', 'seminar_recipients'])
     ]
 
+    sender_email = EmailField('Sender', [DataRequired(), Email()])
     seminar_categories = MultipleItemsField('Seminar categories',
                                             fields=[{'id': 'id', 'caption': 'Category ID', 'required': True}])
     seminar_recipients = EmailListField('Recipients')
@@ -33,6 +38,7 @@ class CERNCronjobsPlugin(IndicoPlugin):
     configurable = True
     settings_form = SettingsForm
     default_settings = {
+        'sender_email': 'noreply-indico-team@cern.ch',
         'seminar_categories': set(),
         'seminar_recipients': set()
     }
