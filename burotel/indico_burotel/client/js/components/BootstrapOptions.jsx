cba4a4b9465b3531d37623c32207f8bf821c68d9
Indico Team <indico-team@cern.ch>
2022-01-05 11:41:24 +0100

    Happy new year 2022 :fireworks:

--- burotel/indico_burotel/client/js/components/BootstrapOptions.jsx
+++ burotel/indico_burotel/client/js/components/BootstrapOptions.jsx
@@ -1,5 +1,5 @@
 // This file is part of the CERN Indico plugins.
-// Copyright (C) 2014 - 2021 CERN
+// Copyright (C) 2014 - 2022 CERN
 //
 // The CERN Indico plugins are free software; you can redistribute
 // them and/or modify them under the terms of the MIT License; see
