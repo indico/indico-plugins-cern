f49249d64f760a5f1d2f5b0ad6a5fba96a520b5a
Adrian Moennich <adrian.moennich@cern.ch>
2024-11-22 17:19:25 +0100

    Add email notifications

--- global/indico_global/cli.py
+++ global/indico_global/cli.py
@@ -6,13 +6,20 @@
 # the LICENSE file for more details.
 
 import sys
+from collections import defaultdict
 from operator import itemgetter
 
 import click
 import yaml
+from sqlalchemy.orm import subqueryload, undefer
 
 from indico.cli.core import cli_group
 from indico.core.db import db
+from indico.core.db.sqlalchemy.principals import PrincipalType
+from indico.core.notifications import make_email, send_email
+from indico.core.plugins import get_plugin_template_module
+from indico.core.settings import SettingsProxyBase
+from indico.modules.categories import Category
 from indico.util.console import verbose_iterator
 
 from indico_global.models.id_map import GlobalIdMap
@@ -41,3 +48,31 @@ def load_mapping(mapping_file):
 
     click.echo('Import finished, committing data...')
     db.session.commit()
+
+
+@cli.command()
+def notify_category_managers():
+    """Notify category managers about upcoming migration."""
+    from indico_global.plugin import GlobalPlugin
+
+    SettingsProxyBase.allow_cache_outside_request = True  # avoid re-querying site_title for every email
+    global_cat = Category.get(GlobalPlugin.settings.get('global_category_id'))
+    query = (global_cat.deep_children_query
+            .filter(~Category.is_deleted, Category.acl_entries.any())
+            .options(subqueryload(Category.acl_entries), undefer('chain_titles')))
+    managers = defaultdict(set)
+    for cat in query:
+        if not (cat_managers := {x.user for x in cat.acl_entries if x.full_access and x.type == PrincipalType.user}):
+            continue
+        for user in cat_managers:
+            managers[user].add(cat)
+
+    for user, cats in managers.items():
+        group_acls = {
+            x.multipass_group_name
+            for cat in cats
+            for x in cat.acl_entries if x.type == PrincipalType.multipass_group
+        }
+        tpl = get_plugin_template_module('emails/cat_notification.txt', name=user.first_name, categories=cats,
+                                         group_acls=group_acls)
+        send_email(make_email(to_list={user.email}, template=tpl))
