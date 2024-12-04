9bf9b53ea25392281aef2f2212dd26564ea6b546
Dominic Hollis <dominic.hollis@cern.ch>
2024-12-04 15:02:38 +0100

    Make lab photo and name clickable

--- labotel/indico_labotel/client/js/components/LabRenderer.jsx
+++ labotel/indico_labotel/client/js/components/LabRenderer.jsx
@@ -6,24 +6,28 @@
 // the LICENSE file for more details.
 
 import React from 'react';
+import PropTypes from 'prop-types';
 import {Item} from 'semantic-ui-react';
 import {Slot, Markdown} from 'indico/react/util';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 
 import SpriteImage from 'indico/modules/rb/components/SpriteImage';
+import {actions as roomActions} from 'indico/modules/rb/common/rooms';
 
 import './LabRenderer.module.scss';
 
-function LabItem({roomInstance}) {
+function LabItem({roomInstance, openRoomDetails}) {
     const {room} = roomInstance.props;
     const {actions} = Slot.split(roomInstance.props.children);
     return (
         <Item key={room.id} styleName="lab-item">
-            <Item.Image size="small" styleName="lab-item-image">
+            <Item.Image size="small" styleName="lab-item-image" onClick={() => openRoomDetails(room.id)} style={{cursor: 'pointer'}}>
                 <SpriteImage pos={room.spritePosition} width="100%" height="100%" />
             </Item.Image>
             <Item.Content>
                 <Item.Header styleName="lab-item-header">
-                    {room.fullName}
+                    <div onClick={() => openRoomDetails(room.id)} style={{cursor: 'pointer'}}>{room.fullName}</div>
                     <div>
                         {actions}
                         {roomInstance.renderFavoriteButton()}
@@ -38,6 +42,22 @@ function LabItem({roomInstance}) {
     );
 }
 
-export default function LabRenderer({roomInstance}) {
-   return <LabItem roomInstance={roomInstance} />;
+LabItem.propTypes = {
+    roomInstance: PropTypes.object.isRequired,
+    openRoomDetails: PropTypes.func.isRequired,
+};
+
+export function LabRenderer({roomInstance, openRoomDetails}) {
+   return <LabItem roomInstance={roomInstance} openRoomDetails={openRoomDetails} />;
 }
+
+LabRenderer.propTypes = {
+    roomInstance: PropTypes.object.isRequired,
+    openRoomDetails: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = dispatch => ({
+    openRoomDetails: bindActionCreators(roomActions.openRoomDetailsBook, dispatch),
+});
+
+export default connect(null, mapDispatchToProps)(LabRenderer);
