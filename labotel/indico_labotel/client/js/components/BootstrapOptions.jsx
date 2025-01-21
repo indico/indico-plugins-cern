6e2eb18bada3c094308f1f1968fd4864d0750bdd
Adrian Moennich <adrian.moennich@cern.ch>
2025-01-21 19:57:34 +0100

    Labotel: Enable favorites filter on landing

--- labotel/indico_labotel/client/js/components/BootstrapOptions.jsx
+++ labotel/indico_labotel/client/js/components/BootstrapOptions.jsx
@@ -10,12 +10,16 @@ import defaultDivisionURL from 'indico-url:plugin_labotel.user_division';
 
 import PropTypes from 'prop-types';
 import React from 'react';
-import {Button} from 'semantic-ui-react';
+import {useSelector} from 'react-redux';
+import {Form, Button, Checkbox} from 'semantic-ui-react';
 
+import {selectors as userSelectors} from 'indico/modules/rb/common/user';
 import {useIndicoAxios} from 'indico/react/hooks';
+import {Translate} from 'indico/react/i18n';
 import {indicoAxios, handleAxiosError} from 'indico/utils/axios';
 
 export default function BootstrapOptions({options: {division}, setOptions}) {
+  const userHasFavorites = useSelector(userSelectors.hasFavoriteRooms);
   const {data: divisions} = useIndicoAxios(divisionsURL());
 
   const handleDivisionClick = async newDivision => {
@@ -28,19 +32,29 @@ export default function BootstrapOptions({options: {division}, setOptions}) {
   };
 
   return (
-    <Button.Group style={{marginBottom: 10}}>
-      {divisions?.map(div => (
-        <Button
-          key={div}
-          onClick={() => handleDivisionClick(division === div ? null : div)}
-          type="button"
-          toggle
-          active={division === div}
-        >
-          {div}
-        </Button>
-      ))}
-    </Button.Group>
+    <>
+      {userHasFavorites && (
+        <Form.Field>
+          <Checkbox
+            label={Translate.string('Search only my favorites')}
+            onClick={(evt, {checked}) => setOptions({onlyFavorites: checked})}
+          />
+        </Form.Field>
+      )}
+      <Button.Group style={{marginBottom: 10}}>
+        {divisions?.map(div => (
+          <Button
+            key={div}
+            onClick={() => handleDivisionClick(division === div ? null : div)}
+            type="button"
+            toggle
+            active={division === div}
+          >
+            {div}
+          </Button>
+        ))}
+      </Button.Group>
+    </>
   );
 }
 
